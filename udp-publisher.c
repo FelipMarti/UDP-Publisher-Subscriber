023c87af1268b2a68a96d777dde042f4a9968b18
Felip Marti <felip.marti.carrillo@gmail.com>
2014-10-21 11:23:33 +0200

    Change to non blocking UDP port

--- udp-publisher.c
+++ udp-publisher.c
@@ -16,7 +16,6 @@ int main(int argc, char**argv)
 
    sockfd=socket(AF_INET,SOCK_DGRAM,0);
 
-   bzero(&servaddr,sizeof(servaddr));
    servaddr.sin_family = AF_INET;
    servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
    servaddr.sin_port=htons(32100);
--- udp-subscriber.c
+++ udp-subscriber.c
@@ -17,18 +17,19 @@ int main(int argc, char**argv)
 
    sockfd=socket(AF_INET,SOCK_DGRAM,0);
 
-   bzero(&servaddr,sizeof(servaddr));
    servaddr.sin_family = AF_INET;
    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
    servaddr.sin_port=htons(32100);
    bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
 
-   for (;;)
+   while (1)
    {
       len = sizeof(cliaddr);
-      n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
-      mesg[n] = 0;
-      printf("Received the following: ");
-      printf("%s",mesg);
+      n = recvfrom(sockfd,mesg,1000,MSG_DONTWAIT,(struct sockaddr *)&cliaddr,&len);
+      if (n>0) {
+          mesg[n] = 0;
+          printf("Received the following: ");
+          printf("%s",mesg);
+      }
    }
 }
